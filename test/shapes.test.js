b69f571acacfcd7e2d307ac4211ec97dd8762d35
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-01-31 12:36:48 -0500

    added square test function to shapes.test.js

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -9,3 +9,12 @@ describe("Triangle", () => {
     expect(shape.render().toEqual("<polygon points='150, 18 244, 182 56, 182' fill='blue'/>"));
   })
 });
+
+// Define square test function
+describe("Square", () => {
+  it("should render a square", () => {
+    const shape = new Square();
+    shape.setColor("green");
+    expect(shape.render().toEqual("<polygon points='100,100 200,100 200,200 100,200' fill='green'/>"));
+  })
+});
