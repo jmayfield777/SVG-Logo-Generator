7f91b42867861dc23da850632c2c16068ad05c2b
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-01-31 15:49:18 -0500

    added inquirer questions to index.js

--- index.js
+++ index.js
@@ -4,4 +4,35 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 // import triangle, square, and circle classes
 const {Triangle, Square, Circle} = require("./lib/shapes.js");
+// variable to hold shape question choices
+const shapeChoices = [
+  "Triangle",
+  "Square",
+  "Circle",
+];
 
+// inquirer questions for users
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter the text (up to 3 characters) you would like the logo to display: ",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter the text color: ",
+    },
+    {
+      type: "input",
+      name: "color",
+      message: "Enter what color you would like the logo to be (color keyword or hexadecimal): ",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "What shape would you like the logo to be?",
+      choices: shapeChoices,
+    },
+]);
