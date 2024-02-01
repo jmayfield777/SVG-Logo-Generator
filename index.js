054e490d3cb636011c75deb2b2bcdc9ba1742528
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-01 13:18:48 -0500

    wrote function for inquirer prompt and finished writetofile function

--- index.js
+++ index.js
@@ -5,62 +5,92 @@ const inquirer = require("inquirer");
 // import triangle, square, and circle classes
 const {Triangle, Square, Circle} = require("./lib/shapes.js");
 // variable to hold shape question choices
-const shapeChoices = [
+const shapeOptions = [
   "Triangle",
   "Square",
   "Circle",
 ];
 
-// inquirer questions for users
-const questions = [
-    { 
-      type: "input",
-      name: "text",
-      message: "Enter the text (up to 3 characters) you would like the logo to display: ",
-    },
-    {
-      type: "input",
-      name: "textColor",
-      message: "Enter the text color: ",
-    },
-    {
-      type: "input",
-      name: "color",
-      message: "Enter what color you would like the logo to be (color keyword or hexadecimal): ",
-    },
-    {
-      type: "list",
-      name: "shape",
-      message: "What shape would you like the logo to be?",
-      choices: shapeChoices,
-    },
-];
-
 // Define function to generate logo.svg
-function generateSvg(data) {
-  // Define svg elements to be rendered
-  return `
-  <svg version="1.1" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
-  ${data.shape}
-  <g>
-  <text x="150" y="130" text-anchor="middle" font-size="40" fill="${data.textColor}>${data.text}</text>
-  </g>
-  </svg>`;
-}
-
-// Define function to writeFile
-function writeToFile(filename, data) {
+function writeToFile(filename, answers) {
   
-  fs.writeFile(filename, data, (err) => {
+  // Define svg elements to be rendered
+  let svgEl = "";
+  let shapeChoice = "";
+
+  // Conditional to check which shape is chosen
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgEl = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <polygon points="150, 18 244, 182 56, 182" fill="${answers.color}"/>
+    <g>
+    <text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>
+    </g>
+    </svg>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgEl = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <polygon points='100,100 200,100 200,200 100,200' fill='${answers.color}'/>
+    <g>
+    <text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>
+    </g>
+    </svg>`;
+  } else if (answers.shape === "Circle") {
+    shapeChoice = new Circle();
+    svgEl = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <circle cx='200' cy='100' r='100' fill='${answers.color}'/>
+    <g>
+    <text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>
+    </g>
+    </svg>`;
+  }
+
+  fs.writeFile(filename, svgEl, (err) => {
     err ? console.log(err) : console.log("Successfully generated SVG!");
   });
 
 }
 
-// function to initialize app
-function init() {
 
-  inquirer.prompt(questions)
-  .then (answers => writeToFile('logo.svg', generateSvg(answers)));
+// inquirer questions for users
+function questionPrompt() {
+
+  inquirer
+    .prompt([
+      { 
+        type: "input",
+        name: "text",
+        message: "Enter the text (up to 3 characters) you would like the logo to display: ",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "Enter the text color: ",
+      },
+      {
+        type: "input",
+        name: "color",
+        message: "Enter what color you would like the logo to be (color keyword or hexadecimal): ",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "What shape would you like the logo to be?",
+        choices: shapeOptions,
+      },
+    ])
+    .then((answers) => {
+
+      if (answers.text.length > 3) {
+        console.log("Please enter 3 characters or less.");
+        return questionPrompt();
+      } else {
+        // call writeToFile()
+        writeToFile("./examples/logo.svg", answers);
+      }
+    });
 
 }
+
+// initialize app
+questionPrompt();
